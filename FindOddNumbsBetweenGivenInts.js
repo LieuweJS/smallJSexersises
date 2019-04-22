9711fdba3054ad16d8de7ec25d8e3492c1a52325
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2019-04-22 14:11:15 +0200

    Update FindOddNumbsBetweenGivenInts.js

--- FindOddNumbsBetweenGivenInts.js
+++ FindOddNumbsBetweenGivenInts.js
@@ -1,1 +1,17 @@
+//find all the odd numbers between x and y
+let answer = findOdd(1, 11001);
+console.log(answer);
 
+function findOdd(y,x) {
+  if(x < y) {
+    return 'X must be bigger than y, please try again with adjusted values.'  
+  } else {
+    let answerArray = [];
+    for(let i = y; i <= x; i++) {
+      if(i%2 !== 0) {
+        answerArray.push(i);
+      }
+    }
+    return answerArray;
+  }
+}
